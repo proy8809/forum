ea5c560158ff4cae71b32d6b245fbcf028b773c3
Patrice Roy <patrice.roy@docupet.com>
2025-03-28 15:40:47 -0400

    feat: implemented Registration form

--- app/src/Twig/Components/RegistrationForm.php
+++ app/src/Twig/Components/RegistrationForm.php
@@ -7,29 +7,36 @@ use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\ValidatableComponentTrait;
 
 #[AsLiveComponent]
 final class RegistrationForm
 {
     use DefaultActionTrait;
+    use ValidatableComponentTrait;
 
     #[LiveProp(writable: true)]
+    #[Assert\Length(min: 8, minMessage: "Username must be at least 8 characters long")]
     public string $username = "";
 
     #[LiveProp(writable: true)]
+    #[Assert\NotBlank(message: "First name can't be empty")]
     public string $firstName = "";
 
     #[LiveProp(writable: true)]
+    #[Assert\NotBlank(message: "Last name can't be empty")]
     public string $lastName = "";
 
     #[LiveProp(writable: true)]
+    #[Assert\Length(min: 8, minMessage: "Password must be at least 8 characters long")]
     public string $password = "";
 
     #[LiveProp(writable: true)]
+    #[Assert\IdenticalTo(propertyPath: "password", message: "Passwords don't match")]
     public string $passwordConfirmation = "";
 
     #[LiveAction]
     public function submit(): void {
-
+        $this->validate();
     }
 }
